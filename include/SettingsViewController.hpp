d5e46c813c75f464f8fb5068978dde2d7c381db2
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-08-18 23:30:55 +0200

    Use BSML for UI and update deps

--- include/SettingsViewController.hpp
+++ include/SettingsViewController.hpp
@@ -1,8 +1,28 @@
+#pragma once
 #include "HMUI/ViewController.hpp"
 
 #include "custom-types/shared/macros.hpp"
+#include "bsml/shared/macros.hpp"
+
 
 DECLARE_CLASS_CODEGEN(InvertedArrows, SettingsViewController, HMUI::ViewController,
-    // Override DidActivate, which is called whenever you enter the menu
+    DECLARE_INSTANCE_FIELD(ListWrapper<StringW>, _directions);
+    DECLARE_INSTANCE_METHOD(ListWrapper<StringW>, get_directions);
+    DECLARE_INSTANCE_METHOD(int, StringWToListIndex, StringW value);
+
     DECLARE_OVERRIDE_METHOD(void, DidActivate, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidActivate", 3), bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+
+    DECLARE_BSML_PROPERTY(bool, enabled);
+    DECLARE_BSML_PROPERTY(bool, alsoFlipBombY);
+    DECLARE_BSML_PROPERTY(bool, useCustomDirections);
+    
+    DECLARE_BSML_PROPERTY(StringW, upDirection);
+    DECLARE_BSML_PROPERTY(StringW, downDirection);
+    DECLARE_BSML_PROPERTY(StringW, leftDirection);
+    DECLARE_BSML_PROPERTY(StringW, rightDirection);
+
+    DECLARE_BSML_PROPERTY(StringW, upLeftDirection);
+    DECLARE_BSML_PROPERTY(StringW, upRightDirection);
+    DECLARE_BSML_PROPERTY(StringW, downLeftDirection);
+    DECLARE_BSML_PROPERTY(StringW, downRightDirection);
 )
--- src/SettingsViewController.cpp
+++ src/SettingsViewController.cpp
@@ -1,103 +1,73 @@
 #include "SettingsViewController.hpp"
 #include "config.hpp"
+#include "assets.hpp"
 
+#include "bsml/shared/BSML.hpp"
+#include "bs-utils/shared/utils.hpp"
 
-#include "UnityEngine/RectOffset.hpp"
-#include "UnityEngine/RectTransform.hpp"
-#include "UnityEngine/Vector2.hpp"
-#include "UnityEngine/UI/Image.hpp"
-#include "UnityEngine/UI/Toggle.hpp"
-#include "UnityEngine/UI/Toggle_ToggleEvent.hpp"
-#include "UnityEngine/UI/LayoutElement.hpp"
-#include "UnityEngine/Events/UnityAction.hpp"
-#include "UnityEngine/Events/UnityAction_1.hpp"
-#include "HMUI/ScrollView.hpp"
-#include "HMUI/ModalView.hpp"
-#include "HMUI/Touchable.hpp"
-
-#include "questui/shared/BeatSaberUI.hpp"
-#include "questui/shared/CustomTypes/Components/ExternalComponents.hpp"
-#include "questui/shared/CustomTypes/Components/Backgroundable.hpp"
-
-#include <cstdlib>
-#include <vector>
-
-using namespace QuestUI;
-using namespace UnityEngine;
-using namespace UnityEngine::UI;
-using namespace UnityEngine::Events;
-using namespace HMUI;
-
+extern ModInfo modInfo;
 extern config_t config;
-
-std::vector<std::string> noteNames = {
-    "Up",
-    "Down",
-    "Left",
-    "Right",
-    "Up Left",
-    "Up Right",
-    "Down Left",
-    "Down Right"
-};
-
 DEFINE_TYPE(InvertedArrows, SettingsViewController);
 
-#define CREATE_NOTESELECTOR(toSelect, name) \
-    dropDown = BeatSaberUI::CreateDropdown(container->get_transform(), name, noteNames_cs[toSelect], noteNames_cs, [](auto value) { \
-        int i = 0; \
-        for (i = 0; i < 8; i++) \
-        { \
-           if (noteNames[i] == value) break; \
-        } \
-        toSelect = i; \
-        SaveConfig(); \
-    });
-
-
-void InvertedArrows::SettingsViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
-{
-    if (firstActivation)
+namespace InvertedArrows {
+    void SettingsViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
     {
-        get_gameObject()->AddComponent<Touchable*>();
-        GameObject* container = BeatSaberUI::CreateScrollableSettingsContainer(get_transform());
-        Toggle* enableToggle = BeatSaberUI::CreateToggle(container->get_transform(), "Enable Inverted Arrows", config.enabled, [](bool value) { 
-                config.enabled = value;
-                SaveConfig();
-                if (config.enabled)
-                {
-                    setenv("InvertedArrowsEnabled", "yes", 1);
-                }
-                else
-                {
-                    unsetenv("InvertedArrowsEnabled");
-                }
-            });
-        BeatSaberUI::AddHoverHint(enableToggle->get_gameObject(), "Wether the mod will change arrow directions at all, enabling this will disable scores from being submitted to scoresaber (if installed)");
-        Toggle* bombFlipToggle = BeatSaberUI::CreateToggle(container->get_transform(), "Also Flip Bomb Y", config.alsoFlipBombY, [](bool value) { 
-                config.alsoFlipBombY = value;
-                SaveConfig();
-            });
-        BeatSaberUI::AddHoverHint(bombFlipToggle->get_gameObject(), "Wether to also invert the bomb layer when flipping the directions");
-        
-        Toggle* customDirToggle = BeatSaberUI::CreateToggle(container->get_transform(), "Use Custom Directions", config.useCustomDirections, [](bool value) { 
-                config.useCustomDirections = value;
-                SaveConfig();
-            });
-        BeatSaberUI::AddHoverHint(customDirToggle->get_gameObject(), "Wether to just invert the directions, or to use the custom directions defined below");
-
-        SimpleTextDropdown* dropDown = nullptr;
-        std::vector<StringW> noteNames_cs = {};
-        for (auto name : noteNames) {
-            noteNames_cs.emplace_back(name);
-        }
-        CREATE_NOTESELECTOR(config.upDirection, u"Up Replacement Direction");
-        CREATE_NOTESELECTOR(config.downDirection, u"Down Replacement Direction");
-        CREATE_NOTESELECTOR(config.leftDirection, u"Left Replacement Direction");
-        CREATE_NOTESELECTOR(config.rightDirection, u"Right Replacement Direction");
-        CREATE_NOTESELECTOR(config.upLeftDirection, u"Up Left Replacement Direction");
-        CREATE_NOTESELECTOR(config.upRightDirection, u"Up Right Replacement Direction");
-        CREATE_NOTESELECTOR(config.downLeftDirection, u"Down Left Replacement Direction");
-        CREATE_NOTESELECTOR(config.downRightDirection, u"Down Right Replacement Direction");
+        if (!firstActivation) return;
+        BSML::parse_and_construct(IncludedAssets::Settings_bsml, get_transform(), this);
     }
+
+    ListWrapper<StringW> SettingsViewController::get_directions() {
+        if (!_directions) {
+            _directions = List<StringW>::New_ctor();
+            _directions->EnsureCapacity(8);
+            _directions->Add("Up");
+            _directions->Add("Down");
+            _directions->Add("Left");
+            _directions->Add("Right");
+            _directions->Add("Up Left");
+            _directions->Add("Up Right");
+            _directions->Add("Down Left");
+            _directions->Add("Down Right");
+        }
+        return _directions;
+    }
+
+    int SettingsViewController::StringWToListIndex(StringW value) {
+        auto dirs = get_directions();
+        int v = dirs->IndexOf(value);
+        return std::max(0, std::min(7, v));
+    }
+
+    bool SettingsViewController::get_enabled() { return config.enabled; }
+    void SettingsViewController::set_enabled(bool value) { 
+        config.enabled = value; 
+        SaveConfig(); 
+        if (value) {
+            bs_utils::Submission::disable(modInfo);
+        } else {
+            bs_utils::Submission::enable(modInfo);
+        }
+    }
+    bool SettingsViewController::get_alsoFlipBombY() { return config.alsoFlipBombY; }
+    void SettingsViewController::set_alsoFlipBombY(bool value) { config.alsoFlipBombY = value; SaveConfig(); }
+    bool SettingsViewController::get_useCustomDirections() { return config.useCustomDirections; }
+    void SettingsViewController::set_useCustomDirections(bool value) { config.useCustomDirections = value; SaveConfig(); }
+    
+    StringW SettingsViewController::get_upDirection() { return get_directions()[config.upDirection]; }
+    void SettingsViewController::set_upDirection(StringW value) { config.upDirection = StringWToListIndex(value); SaveConfig(); }
+    StringW SettingsViewController::get_downDirection() { return get_directions()[config.downDirection]; }
+    void SettingsViewController::set_downDirection(StringW value) { config.downDirection = StringWToListIndex(value); SaveConfig(); }
+    StringW SettingsViewController::get_leftDirection() { return get_directions()[config.leftDirection]; }
+    void SettingsViewController::set_leftDirection(StringW value) { config.leftDirection = StringWToListIndex(value); SaveConfig(); }
+    StringW SettingsViewController::get_rightDirection() { return get_directions()[config.rightDirection]; }
+    void SettingsViewController::set_rightDirection(StringW value) { config.rightDirection = StringWToListIndex(value); SaveConfig(); }
+    
+    StringW SettingsViewController::get_upLeftDirection() { return get_directions()[config.upLeftDirection]; }
+    void SettingsViewController::set_upLeftDirection(StringW value) { config.upLeftDirection = StringWToListIndex(value); SaveConfig(); }
+    StringW SettingsViewController::get_upRightDirection() { return get_directions()[config.upRightDirection]; }
+    void SettingsViewController::set_upRightDirection(StringW value) { config.upRightDirection = StringWToListIndex(value); SaveConfig(); }
+    StringW SettingsViewController::get_downLeftDirection() { return get_directions()[config.downLeftDirection]; }
+    void SettingsViewController::set_downLeftDirection(StringW value) { config.downLeftDirection = StringWToListIndex(value); SaveConfig(); }
+    StringW SettingsViewController::get_downRightDirection() { return get_directions()[config.downRightDirection]; }
+    void SettingsViewController::set_downRightDirection(StringW value) { config.downRightDirection = StringWToListIndex(value); SaveConfig(); }
 }
